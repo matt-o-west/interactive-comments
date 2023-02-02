a481d2a5789770772a1ab0b18cc9894dab41059b
matt-o-west <matt.omalley.west@gmail.com>
2023-02-01 21:05:01 -0500

    add redux store

---
+++ src/redux/commentSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const commentSlice = () =>
+  createSlice({
+    name: 'comments',
+    initialState: [],
+    reducers: {
+      addComment: (state, action) => {
+        state.push(action.payload)
+      },
+      removeComment: (state, action) => {
+        return state.filter((comment) => comment.id !== action.payload)
+      },
+      editComment: (state, action) => {
+        const { id, newComment } = action.payload
+        const existingComment = state.find((comment) => comment.id === id)
+        if (existingComment) {
+          existingComment.comment = newComment
+        }
+      },
+    },
+  })
+
+export default commentSlice
