9b7f0ea2eddc594a5d41bc3517eccebb215cf73b
matt-o-west <matt.omalley.west@gmail.com>
2023-02-02 22:16:38 -0500

    work on actions and adding initial state

---
+++ src/components/BaseInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BaseInput = () => {
+  return (
+    <div>
+      <form>
+        <img src='../assets/avatars/image-ramsesmiron.png' alt='avatar image' />
+        <input type='submit' value='Submit' placeholder='Add a comment...' />
+      </form>
+    </div>
+  )
+}
+
+export default BaseInput
