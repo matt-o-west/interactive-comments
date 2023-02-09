5fd4a1175b895e055b2c2651f9db4486f2498144
matt-o-west <matt.omalley.west@gmail.com>
2023-02-08 21:50:55 -0500

    working on counter action/logic

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Counter from './Counter'
+import { addComment, removeComment, editComment } from '../redux/commentSlice'
 
 const Comment = ({ content, createdAt, score, user, replies }) => {
   const { username } = user
-  console.log(username)
+  console.log(score)
 
   return (
     <>
       <div className='grid grid-cols-3 grid-rows-2'>
         <div className='col-span-1 row-span-2'>
-          <Counter>{score}</Counter>
+          <Counter score={score}>{score}</Counter>
         </div>
         <div className='col-span-1 row-span-1'>
           <img
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { incrementScore, decrementScore } from '../redux/commentSlice'
 
-const Counter = ({ children, id }) => {
-  const comment = useSelector((state) =>
-    state.commentReducer[0].comments.find((comment) => comment.id === id)
-  )
+const Counter = ({ children, id, score }) => {
   const dispatch = useDispatch()
+  console.log(score)
 
   return (
     <div className='w-full bg-slate-400 rounded-md'>
-      <button onClick={() => dispatch(incrementScore())}>
+      <button
+        onClick={() => dispatch(incrementScore({ id: id, score: score }))}
+      >
         <span className='font-bold text-slate-700'>+</span>
       </button>
       <input type='text' value={children} readOnly />
-      <button onClick={() => dispatch(decrementScore())}>
+      <button
+        onClick={() => dispatch(decrementScore({ id: id, score: score }))}
+      >
         <span className='font-bold text-slate-700'>-</span>
       </button>
     </div>
