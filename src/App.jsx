811a638aa71122b9c39d89e0ce978a0e9fb86305
matt-o-west <matt.omalley.west@gmail.com>
2023-02-08 20:43:24 -0500

    update assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
 import store from './redux/commentStore'
